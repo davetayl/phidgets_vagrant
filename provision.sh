358275ed76256243c94ba0f57f7c7b5d8c5e8e72
David Taylor <dave@imagetaylor.com>
2020-01-07 18:41:52 +0100

    Updated debian image to v9, v10 not currently supported.

--- provision.sh
+++ provision.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 # Base OS update
-/sbin/apt-get update > /dev/null 2>&1
-/sbin/apt-get -y upgrade > /dev/null 2>&1
-/sbin/apt-get -y install wget > /dev/null 2>&1
+/usr/bin/apt-get update
+/usr/bin/apt-get -y upgrade 
 
 # Add Phidgets repos
-wget -qO- http://www.phidgets.com/gpgkey/pubring.gpg | apt-key add - > /dev/null 2>&1
-echo 'deb http://www.phidgets.com/debian stretch main' > /etc/apt/sources.list.d/phidgets.list > /dev/null 2>&1
+/usr/bin/wget -qO- http://www.phidgets.com/gpgkey/pubring.gpg | apt-key add - 
+/usr/bin/echo 'deb http://www.phidgets.com/debian stretch main' > /etc/apt/sources.list.d/phidgets.list
 
 # Install Phidgets libraries
-apt-get update > /dev/null 2>&1
-apt-get install libphidget22 > /dev/null 2>&1
+/usr/bin/apt-get update
+/usr/bin/apt-get -y install libphidget22 
