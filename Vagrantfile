65638fbdb5950e6eab57e25d3e9ce3c066dfec71
David Taylor <dave@imagetaylor.com>
2020-01-07 21:42:31 +0100

    Updated box, there is an ongoing issue with doing a full update as part of the provisioning process involving grub. For the moment a full update requires operator intervention, as such the update line has been commented out

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,4 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/debian-9"
+  config.vm.box = "remram/debian-9-i386"
   config.vm.provision "shell", path: "./provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Set system name
+hostnamectl set-hostname phidgetdev
+
 # Base OS update
 /usr/bin/apt-get update
-/usr/bin/apt-get -y upgrade 
+# /usr/bin/apt-get -fy upgrade
 
 # Add Phidgets repos
-/usr/bin/wget -qO- http://www.phidgets.com/gpgkey/pubring.gpg | apt-key add - 
-/usr/bin/echo 'deb http://www.phidgets.com/debian stretch main' > /etc/apt/sources.list.d/phidgets.list
+/usr/bin/wget -qO- http://www.phidgets.com/gpgkey/pubring.gpg | apt-key add -
+/bin/echo 'deb http://www.phidgets.com/debian stretch main' > /etc/apt/sources.list.d/phidgets.list
 
 # Install Phidgets libraries
 /usr/bin/apt-get update
