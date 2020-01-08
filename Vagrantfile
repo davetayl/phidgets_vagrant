1df881d32574665c9a9cf45d9f8bb242def7b9f4
David Taylor <dave@imagetaylor.com>
2020-01-08 19:12:50 +0100

    New box uploaded and included, all tested and pretty, go nuts. As usual, send any complaints to dev@null.com

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "remram/debian-9-i386"
+  config.vm.box = "hebari/debian-stretch-i386"
+  config.vm.box_version = "0.0.1"
   config.vm.provision "shell", path: "./provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
 # Set system name
-hostnamectl set-hostname phidgetdev
-/bin/sed -i "s/debian-9rc1-i386/phidgetdev/g" /etc/hosts
-echo "Name set..."
-echo ""
-echo ""
-echo ""
+hostnamectl set-hostname phidgetdev > /dev/null 2>&1
+/bin/sed -i "s/debianvb/phidgetdev/g" /etc/hosts > /dev/null 2>&1
+echo "Name set...(Pow!)"
 
 # Base OS update
 /usr/bin/apt-get update > /dev/null 2>&1
-# /usr/bin/apt-get -y upgrade
-echo "OS Updated..."
-echo ""
-echo ""
-echo ""
+/usr/bin/apt-get -y upgrade > /dev/null 2>&1
+echo "OS Updated...(Wak!)"
 
 # Add Phidgets repos
 /usr/bin/wget -qO- http://www.phidgets.com/gpgkey/pubring.gpg | apt-key add - > /dev/null 2>&1
@@ -23,10 +17,7 @@ echo ""
 # Install Phidgets libraries
 /usr/bin/apt-get update > /dev/null 2>&1
 /usr/bin/apt-get -y install libphidget22 > /dev/null 2>&1
-echo "Phidgets installed..."
-echo ""
-echo ""
-echo ""
+echo "Phidgets installed...(Kerthewie!)"
 echo "----------------------------"
 echo "Sweet bro, you're up as bro!"
 echo "----------------------------"
